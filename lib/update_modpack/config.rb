da8a05c911a3fac92675ca7e6e49f572e5367ef3
Alex Monroe <alex@monroepost.com>
2026-03-26 08:24:23 -0400

    Fix Windows profile dir

--- lib/update_modpack/config.rb
+++ lib/update_modpack/config.rb
@@ -4,8 +4,8 @@ module UpdateModpack
   REPO_DIR = File.expand_path("../..", __dir__)
   MODPACKS_DIR = File.join(REPO_DIR, "modpacks")
   PROFILES_DIR = if ENV.key?("WSL_DISTRO_NAME")
-    windows_home = `wslpath "#{ENV["USERPROFILE"]}"`.strip
-    File.join(windows_home, "AppData/Roaming/ModrinthApp/profiles")
+    # requires `setx WSLENV USERPROFILE/up`
+    File.join(ENV["USERPROFILE"], "AppData/Roaming/ModrinthApp/profiles")
   else
     File.expand_path("~/Library/Application Support/ModrinthApp/profiles")
   end
