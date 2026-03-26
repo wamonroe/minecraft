e12e51730ff6996c9aac19a34c22c1de158949cc
Alex Monroe <alex@monroepost.com>
2026-03-26 06:21:35 -0400

    Support Windows/WSL

--- lib/update_modpack/config.rb
+++ lib/update_modpack/config.rb
@@ -3,6 +3,11 @@
 module UpdateModpack
   REPO_DIR = File.expand_path("../..", __dir__)
   MODPACKS_DIR = File.join(REPO_DIR, "modpacks")
-  PROFILES_DIR = File.expand_path("~/Library/Application Support/ModrinthApp/profiles")
+  PROFILES_DIR = if ENV.key?("WSL_DISTRO_NAME")
+    windows_home = `wslpath "#{ENV["USERPROFILE"]}"`.strip
+    File.join(windows_home, "AppData/Roaming/ModrinthApp/profiles")
+  else
+    File.expand_path("~/Library/Application Support/ModrinthApp/profiles")
+  end
   MANAGED_DIRS = %w[mods resourcepacks shaderpacks].freeze
 end
